a32dababf9af798b44f50a7196e3dcfda95e0df7
Nick <nickpassarella@gmail.com>
2016-12-30 00:20:02 -0500

    Todos reducer complete with tests passing.

--- src/reducers/todosReducer.js
+++ src/reducers/todosReducer.js
@@ -1,7 +1,30 @@
 const initialState = [];
 
+let todoCounter = 1;
+
+const newTodo = text => ({
+  text: text,
+  id: todoCounter++,
+  completed: false
+});
+
+const toggleTodo = (todo, actionId) => {
+  if (todo.id !== actionId) {
+    return todo;
+  } else {
+    return {
+      ...todo,
+      completed: !todo.completed
+    };
+  }
+};
+
 export default function todosReducer(state = initialState, action) {
   switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, newTodo(action.text)];
+    case 'TOGGLE_TODO':
+      return state.map(todo => toggleTodo(todo, action.id));
     default:
       return state;
   }
--- test/reducers/todosReducerTest.js
+++ test/reducers/todosReducerTest.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import deepFreeze from 'deep-freeze';
 import * as actions from '../../src/actions/todoActions';
 import reducer from '../../src/reducers/todosReducer';
 
@@ -6,44 +7,50 @@ describe('Todos Reducer', () => {
   it('returns default state if no current state is passed', () => {
     expect(reducer(undefined, {})).to.deep.equal([])
   });
+
   it('can add a first todo', () => {
+    let beforeState = [];
     let afterState = [{ id: 1, text: 'do it!', completed: false }];
+    deepFreeze(beforeState);
 
-    expect(reducer([], actions.addTodo('do it!'))).to.deep.equal(afterState);
+    expect(reducer(beforeState, actions.addTodo('do it!'))).to.deep.equal(afterState);
   });
-  xit('can add a second todo', () => {
-    let beforeState = [{ id: 1, text: 'do it!' }];
 
+  it('can add a second todo', () => {
+    let beforeState = [{ id: 1, text: 'do it!', completed: false }];
     let afterState = [
       { id: 1, text: 'do it!', completed: false },
       { id: 2, text: 'just do it!', completed: false }
     ];
+    deepFreeze(beforeState);
 
     expect(reducer(beforeState, actions.addTodo('just do it!'))).to.deep.equal(afterState);
   });
-  xit('can toggle an incomplete todo to be completed', () => {
+
+  it('can toggle an incomplete todo to be completed', () => {
     let beforeState = [
       { id: 1, text: 'do it!', completed: false },
       { id: 2, text: 'just do it!', completed: false }
     ];
-
     let afterState = [
       { id: 1, text: 'do it!', completed: true },
       { id: 2, text: 'just do it!', completed: false }
     ];
+    deepFreeze(beforeState);
 
     expect(reducer(beforeState, actions.toggleTodo(1))).to.deep.equal(afterState);
   });
-  xit('can toggle an complete todo to be incomplete', () => {
+
+  it('can toggle an complete todo to be incomplete', () => {
     let beforeState = [
       { id: 1, text: 'do it!', completed: true },
       { id: 2, text: 'just do it!', completed: false }
     ];
-
     let afterState = [
       { id: 1, text: 'do it!', completed: false },
       { id: 2, text: 'just do it!', completed: false }
     ];
+    deepFreeze(beforeState);
 
     expect(reducer(beforeState, actions.toggleTodo(1))).to.deep.equal(afterState);
   });
